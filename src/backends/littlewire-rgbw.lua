fae85849db1b94308cab71469b75558bc0addb1f
Evey Quirk <ev@7pr.xyz>
2017-01-02 03:19:09 +0000

    wip

---
+++ src/backends/littlewire-rgbw.lua
@@ -0,0 +1,31 @@
+local Lwdev = {}
+
+function Lwdev:init(device)
+  self.littlewire = require("littlewire")
+
+  local littlewire_devices = littlewire.search()
+
+  print("Found ", #littlewire_devices, " littlewire devices.\n")
+
+  self.lw_dev = littlewire.connect()
+
+  print("Connected to first device.")
+  print("FW Version: ", littlewire.readFirmwareVersion(self.lw_dev), "\n")
+
+  littlewire.softPWM_state(self.lw_dev, 1)
+
+  print("Littlewire rgbw backend initialized.")
+end
+
+-- FIXME In RGBW mode we are overloading every other pixel to carry W data
+function Lwdev:set_pixels(pixels) 
+  local w
+  if #pixels == 2 then
+    w = pixels[2][1]
+  else
+    w = 0
+  end
+  littlewire.softPWM_write(self.lw_dev, pixels[1][1], pixels[1][2], pixels[1][3], w)
+end
+
+return Lwdev
